757e8572ab5ab1185c8179798c031a2baaed0c40
Pravitha Pratheep <pratheepp@spu.edu>
2021-04-07 18:32:07 -0700

    Added more files to constant , iamges and icons

--- App.js
+++ App.js
@@ -1,21 +1,70 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View,ImageBackground } from 'react-native';
+import Background from './assets/landing.png';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={[styles.container, {
+        flexDirection: "column"
+      }]}>
+        
+        <ImageBackground source={Background} style={styles.backgroundImage}>
+        <View style ={{flex: 3}} />
+        <Text style={styles.h1}>Basket</Text>
+        <View style ={{flex: 1}} />
+        {<Button
+        title="LOGIN"
+        style={styles.button}
+        onPress={() => this.onPress()}
+        
+        /> }
+         <View style ={{flex: 1}} />
+        {<CreateButton
+        title="Create an Account"
+        style={styles.button}
+        onPress={() => this.onPress()}
+        
+        /> }
+        </ImageBackground>
+        
+        
     </View>
   );
 }
+}
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'transparent',
+    //alignItems: 'center',
+    //justifyContent: 'space-between',
+    //width: '100%',
   },
+  backgroundImage: {
+    resizeMode: 'cover',
+    justifyContent: 'center',
+    width: '100%',
+    height: '100%',
+  },
+  h1: {
+    color: '#FF9E45',
+    fontSize:60,
+    fontWeight: "bold",
+    justifyContent: "center",
+    textAlign:'center',
+     
+    
+
+  },
+   CreateButton: {
+     color: 'transparent',
+     justifyContent: "center",
+     borderRadius: 20,
+     borderColor: '#FF9E45',
+    
+   }
 });
---
+++ constants/theme.js
@@ -0,0 +1,62 @@
+import { Dimensions } from "react-native";
+const { width, height } = Dimensions.get("window");
+
+export const COLORS = {
+    // base colors
+    primary: "#FF9E45", // orange
+    secondary: "#FFFFFF",   // white
+
+    // colors
+    black: "#1E1F20",
+    white: "#FFFFFF",
+    darkOrange: "#E56E00",
+    dropShadow: "#FF7A00",
+    transparent: "transparent",
+    darkGray: '#6D6D6D',
+    lightGray: '#EBEBEB,'
+};
+
+export const SIZES = {
+    // global sizes
+    base: 8,
+    font: 14,
+    cornerRadius: 22,
+    padding: 10,
+    padding2: 12,
+
+    // font sizes
+    largeTitle: 72, // landing
+    h1: 30, //text in buttons
+    h2: 60, // Basket , Confirmation , Hello John, Sign Up , Login
+    body1: 18, // registration page , departments,address
+    body2: 30, // Your basket is empty
+    body3: 18, // subtotal and subtotal price
+    body4: 16, // price in store
+    body5: 12, // item name and count
+    body6: 18, // add to basket button in item page
+    body7: 16, // Item name in chekcout page
+    body8: 12, // tab
+    
+
+    // app dimensions
+    width,
+    height
+};
+
+export const FONTS = {
+    largeTitle: { fontFamily: "SignikaNegative-Bold", fontSize: SIZES.largeTitle, lineHeight: 55 },
+    h1: { fontFamily: "SignikaNegative-Bold", fontSize: SIZES.h1, lineHeight: 36 },
+    h2: { fontFamily: "SignikaNegative-Bold", fontSize: SIZES.h2, lineHeight: 30 },
+    body1: { fontFamily: "SignikaNegative-SemiBold", fontSize: SIZES.body1, lineHeight: 22 },
+    body2: { fontFamily: "SignikaNegative-SemiBold", fontSize: SIZES.body2, lineHeight: 22 },
+    body3: { fontFamily: "SignikaNegative-Bold", fontSize: SIZES.body3, lineHeight: 22 },
+    body4: { fontFamily: "SignikaNegative-SemiBold", fontSize: SIZES.body4, lineHeight: 36 },
+    body5: { fontFamily: "SignikaNegative-Regular", fontSize: SIZES.body5, lineHeight: 22 },
+    body6: { fontFamily: "SignikaNegative-Regular", fontSize: SIZES.body6, lineHeight: 22 },
+    body7: { fontFamily: "SignikaNegative-Regular", fontSize: SIZES.body7, lineHeight: 22 },
+    body8: { fontFamily: "SignikaNegative-SemiBold", fontSize: SIZES.body8, lineHeight: 22 },
+};
+
+const appTheme = { COLORS, SIZES, FONTS };
+
+export default appTheme;

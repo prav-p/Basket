a725b13818eae42ca6adf184c941ea888498b1fe
Pravitha Pratheep <pratheepp@spu.edu>
2021-04-11 23:52:16 -0700

    Working landing page , but no navigation yet

--- screens/Landing.js
+++ screens/Landing.js
@@ -4,8 +4,9 @@ import {
     Text,
     ImageBackground,
     StyleSheet,
+    TouchableOpacity,
 } from "react-native";
-import { images } from "../constants";
+import { COLORS, images,FONTS } from "../constants";
 
 const Landing = () =>{
     return (
@@ -13,6 +14,26 @@ const Landing = () =>{
         style={style.img}
         source = {images.landing}>
 
+        
+            <View style = {style.title}>
+                <Text style = {style.text3}>Basket</Text>
+            </View>
+       
+        <TouchableOpacity>
+            <View style={style.btn1}>
+                <View style={style.insidebtn}>
+                    <Text style = {style.text}>Login</Text>
+                </View>
+            </View>
+            </TouchableOpacity>  
+
+            <TouchableOpacity>
+            <View style={style.btn2}>
+                <View style={style.insidebtn2}>
+                    <Text style = {style.text2}> Create An Account</Text>
+                </View>
+            </View>
+            </TouchableOpacity>  
         </ImageBackground>
     )
 }
@@ -21,7 +42,63 @@ const style = StyleSheet.create ({
     img: {
         flex: 1,
         resizeMode:"stretch",
+    },
+    btn1: {
+        alignItems: 'center',
+        marginTop: '156%'
+    },
+    insidebtn: {
+        height: 62,
+        width: 362,
+        textAlign: 'center',
+        backgroundColor: COLORS.primary,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 20
+    },
+    text: {
+        fontFamily: "SignikaNegative-Bold",
+        textAlign: 'center',
+        fontSize: 30,
+        color: COLORS.white,
+    },
+    btn2: {
+        alignItems: 'center',
+        marginTop: '10%'
+    },
+    insidebtn2: {
+        height: 62,
+        width: 362,
+        textAlign: 'center',
+        backgroundColor: COLORS.white,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 20,
+        borderWidth:2,
+        borderColor: COLORS.primary,
+    },
+    text2: {
+        fontFamily: "SignikaNegative-Bold",
+        textAlign: 'center',
+        fontSize: 30,
+        color: COLORS.primary,
+    },
+    title: {
+        position: 'absolute',
+        top: 178,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    text3: {
+        fontFamily: "SignikaNegative-Bold",
+        textAlign: 'center',
+        fontSize: 72,
+        color: COLORS.primary,
     }
+
 })
 
 export default Landing;

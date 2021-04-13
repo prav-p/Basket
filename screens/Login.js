a2123dbdc60d39cad3476b3d392740f6ef86e415
richardlwspu14 <56092710+richardlwspu14@users.noreply.github.com>
2021-04-13 12:36:41 -0700

    Update Login.js

--- screens/Login.js
+++ screens/Login.js
@@ -1,15 +1,56 @@
-import React from "react";
+import React from 'react';
 import {
+    SafeAreaView,
     View,
-    Text
+    Text,
+    StyleSheet,
+    TouchableOpacity,
+    Image,
+    FlatList
 } from "react-native";
 
-const Login = () =>{
+const Login = () => {
+    function renderLogin() {
+        return (
+            <View>
+                <Text style={styles.textStyle}>LOGIN</Text>
+            </View>
+        )
+    }
+
     return (
-        <View>
-            <Text>Login</Text>
-        </View>
+        <SafeAreaView>
+            {renderLogin()}
+        </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    textStyle: {
+        position: 'absolute',
+        width: 362,
+        height: 29,
+        left: 26,
+        top: 110,
+
+        fontFamily: 'SignikaNegative-Bold',
+        fontStyle: 'normal',
+        fontWeight: 'bold',
+        fontSize: 60,
+        lineHeight: 74,
+        display: 'flex',
+        alignItems: 'center',
+        textAlign: 'center',
+        letterSpacing: 0.005,
+
+        color: '#FF9E45'
+    }
+})
 
 export default Login;

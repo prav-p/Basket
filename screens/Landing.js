0f7d8d3d5a4655029b052d559e229c71ceabf0ae
Pravitha Pratheep <pratheepp@spu.edu>
2021-04-12 17:11:22 -0700

    Fixed issue of buttons on landing page

--- screens/Landing.js
+++ screens/Landing.js
@@ -1,106 +1,103 @@
 import React from "react";
 import {
-    View,
-    Text,
-    ImageBackground,
-    StyleSheet,
-    TouchableOpacity,
+  View,
+  Text,
+  ImageBackground,
+  StyleSheet,
+  TouchableOpacity,
 } from "react-native";
-import { useNavigation } from '@react-navigation/native';
-import { COLORS, images, } from "../constants";
+import { useNavigation } from "@react-navigation/native";
+import { COLORS, images } from "../constants";
 
-const Landing = () =>{
-    const navigation = useNavigation(); 
-    return (
-        <ImageBackground
-        style={style.img}
-        source = {images.landing}>
+const Landing = () => {
+  const navigation = useNavigation();
+  return (
+    <ImageBackground style={style.img} source={images.landing}>
+      <View style={style.title}>
+        <Text style={style.text3}>Basket</Text>
+      </View>
 
-        
-            <View style = {style.title}>
-                <Text style = {style.text3}>Basket</Text>
-            </View>
-       
-        <TouchableOpacity onPress={()=>navigation.navigate('Login')}>
-            <View style={style.btn1}>
-                <View style={style.insidebtn}>
-                    <Text style = {style.text}>Login</Text>
-                </View>
-            </View>
-            </TouchableOpacity>  
+      <TouchableOpacity onPress={() => navigation.navigate("Login")}>
+        <View style={style.btn1}>
+          <View style={style.insidebtn}>
+            <Text style={style.text}>Login</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
 
-            <TouchableOpacity onPress={()=>navigation.navigate('Create An Account')}>
-            <View style={style.btn2}>
-                <View style={style.insidebtn2}>
-                    <Text style = {style.text2}> Create An Account</Text>
-                </View>
-            </View>
-            </TouchableOpacity>  
-        </ImageBackground>
-    )
-}
+      <TouchableOpacity
+        onPress={() => navigation.navigate("Create An Account")}
+      >
+        <View style={style.btn2}>
+          <View style={style.insidebtn2}>
+            <Text style={style.text2}> Create An Account</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+    </ImageBackground>
+  );
+};
 
-const style = StyleSheet.create ({
-    img: {
-        flex: 1,
-        resizeMode:"stretch",
-    },
-    btn1: {
-        alignItems: 'center',
-        marginTop: '156%'
-    },
-    insidebtn: {
-        height: 62,
-        width: 362,
-        textAlign: 'center',
-        backgroundColor: COLORS.primary,
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderRadius: 20
-    },
-    text: {
-        fontFamily: "SignikaNegative-Bold",
-        textAlign: 'center',
-        fontSize: 30,
-        color: COLORS.white,
-    },
-    btn2: {
-        alignItems: 'center',
-        marginTop: '10%'
-    },
-    insidebtn2: {
-        height: 62,
-        width: 362,
-        textAlign: 'center',
-        backgroundColor: COLORS.white,
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderRadius: 20,
-        borderWidth:2,
-        borderColor: COLORS.primary,
-    },
-    text2: {
-        fontFamily: "SignikaNegative-Bold",
-        textAlign: 'center',
-        fontSize: 30,
-        color: COLORS.primary,
-    },
-    title: {
-        position: 'absolute',
-        top: 178,
-        left: 0,
-        right: 0,
-        bottom: 0,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    text3: {
-        fontFamily: "SignikaNegative-Bold",
-        textAlign: 'center',
-        fontSize: 72,
-        color: COLORS.primary,
-    }
-
-})
+const style = StyleSheet.create({
+  img: {
+    flex: 1,
+    resizeMode: "stretch",
+  },
+  btn1: {
+    alignItems: "center",
+    top: 560,
+  },
+  insidebtn: {
+    height: 62,
+    width: 362,
+    textAlign: "center",
+    backgroundColor: COLORS.primary,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 20,
+  },
+  text: {
+    fontFamily: "SignikaNegative-Bold",
+    textAlign: "center",
+    fontSize: 30,
+    color: COLORS.white,
+  },
+  btn2: {
+    alignItems: "center",
+    top: 600,
+  },
+  insidebtn2: {
+    height: 62,
+    width: 362,
+    textAlign: "center",
+    backgroundColor: COLORS.white,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 20,
+    borderWidth: 2,
+    borderColor: COLORS.primary,
+  },
+  text2: {
+    fontFamily: "SignikaNegative-Bold",
+    textAlign: "center",
+    fontSize: 30,
+    color: COLORS.primary,
+  },
+  title: {
+    position: "absolute",
+    top: 178,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text3: {
+    fontFamily: "SignikaNegative-Bold",
+    textAlign: "center",
+    fontSize: 72,
+    color: COLORS.primary,
+  },
+});
 
 export default Landing;

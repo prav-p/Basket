108789bf1c2cceae9e58db6e7529d84e6e1b2feb
Pravitha Pratheep <pratheepp@spu.edu>
2021-04-13 17:59:09 -0700

    -

--- App.js
+++ App.js
@@ -1,45 +1,49 @@
-import React from 'react';
-import { createStackNavigator} from "@react-navigation/stack";
-import {NavigationContainer} from "@react-navigation/native";
-import { useFonts } from 'expo-font';
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import { useFonts } from "expo-font";
 
-import Tabs from './navigation/tabs';
-import {Home,Item,Login,Landing,Search,Checkout,CreateAccount} from './screens';
+import Tabs from "./navigation/tabs";
+import {
+  Home,
+  Item,
+  Login,
+  Landing,
+  Search,
+  Checkout,
+  CreateAccount,
+} from "./screens";
 
 const Stack = createStackNavigator();
 
 const App = () => {
-  
   const [loaded] = useFonts({
-    "SignikaNegative-Bold" : require('./assets/fonts/SignikaNegative-Bold.ttf'),
-    "SignikaNegative-SemiBold" : require('./assets/fonts/SignikaNegative-SemiBold.ttf'),
-    "SignikaNegative-Regular" : require('./assets/fonts/SignikaNegative-Regular.ttf'),
+    "SignikaNegative-Bold": require("./assets/fonts/SignikaNegative-Bold.ttf"),
+    "SignikaNegative-SemiBold": require("./assets/fonts/SignikaNegative-SemiBold.ttf"),
+    "SignikaNegative-Regular": require("./assets/fonts/SignikaNegative-Regular.ttf"),
+  });
 
-  })
-  
-  if(!loaded){
+  if (!loaded) {
     return null;
   }
 
-  return(
+  return (
     <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{
-            headerShown: false
-          }}
-          initialRouteName={"Landing"}
-          >
-          <Stack.Screen name="Basket" component={Checkout} />
-          <Stack.Screen name="Landing" component={Landing} />
-          <Stack.Screen name="Asian Family Mar" component={Tabs} />
-          <Stack.Screen name="Home" component={Home} /> 
-          <Stack.Screen name="Login" component={Login} />
-          <Stack.Screen name="Create An Account" component={CreateAccount} />
-          
-          
-        </Stack.Navigator>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+        initialRouteName={"Landing"}
+      >
+        <Stack.Screen name="Basket" component={Checkout} />
+        <Stack.Screen name="Landing" component={Landing} />
+        <Stack.Screen name="Asian Family Mart" component={Tabs} />
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Create An Account" component={CreateAccount} />
+      </Stack.Navigator>
     </NavigationContainer>
-  )
-}
+  );
+};
 
 export default App;

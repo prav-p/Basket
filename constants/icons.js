05ec9d79828179e1d740b34ce60a3d4b55ad8776
Pravitha Pratheep <pratheepp@spu.edu>
2021-04-20 23:50:59 -0700

    Working on search bar component

--- constants/icons.js
+++ constants/icons.js
@@ -1,9 +1,11 @@
 export const basket = require("../assets/icons/Basket.png");
 export const search = require("../assets/icons/Search.png");
 export const home = require("../assets/icons/Home.png");
+export const basketG = require("../assets/icons/BasketGreen.png");
 
-export default{
-    basket,
-    search,
-    home
-}
+export default {
+  basket,
+  search,
+  home,
+  basketG,
+};
--- screens/Home.js
+++ screens/Home.js
@@ -1,15 +1,92 @@
 import React from "react";
+import { COLORS, icons } from "../constants";
 import {
-    View,
-    Text
+  SafeAreaView,
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  Icon,
+  Keyboard,
+  FlatList,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  TextInput,
 } from "react-native";
+import { SearchBar } from "react-native-elements";
+import { useNavigation } from "@react-navigation/native";
+import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
 
-const Home = () =>{
+const Home = () => {
+  const navigation = useNavigation();
+
+  function renderBasket() {
     return (
-        <View>
-            <Text>HELLO</Text>
-        </View>
-    )
-}
+      <View style={style.basket}>
+        <TouchableOpacity onPress={() => navigation.navigate("Basket")}>
+          <Image source={icons.basketG}></Image>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+  function renderGreeting() {
+    return (
+      <View style>
+        <Text style={style.greetingText}>Hello there</Text>
+      </View>
+    );
+  }
+  function renderSearchBar() {
+    state = {
+      search: '',
+    };
+    updateSearch = (search) => {
+      this.setState({ search });
+    };
+    render() {
+      const { search } = this.state;
+  
+      return (
+        <SearchBar
+          placeholder="Type Here..."
+          onChangeText={this.updateSearch}
+          value={search}
+        />
+      );
+    }
+  }
+  return (
+    <SafeAreaView>
+      {renderGreeting()}
+      {renderBasket()}
+      {renderSearchBar()}
+    </SafeAreaView>
+  );
+};
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.primary,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  basket: {
+    flex: 1,
+    left: 300,
+    top: -55,
+  },
+  greetingText: {
+    top: 70,
+    fontFamily: "SignikaNegative-Bold",
+    textAlign: "center",
+    fontSize: RFValue(70, 896),
+    color: COLORS.primary,
+  },
+});
 
 export default Home;

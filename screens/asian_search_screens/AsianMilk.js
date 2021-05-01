6a993530307fd819208b202f204007e72502b75e
richardlwspu14 <56092710+richardlwspu14@users.noreply.github.com>
2021-04-30 20:10:13 -0700

    tried to add AsianMilk to index.js

---
+++ screens/asian_search_screens/AsianMilk.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {
+    View,
+    Text
+} from "react-native";
+
+const AsianMilk = () => {
+    return (
+        <View>
+            <Text>AsianMilk</Text>
+        </View>
+    )
+}
+
+export default AsianMilk;

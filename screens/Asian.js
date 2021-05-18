308a19fe0d41996492c1509b7a354bc43f46f4c9
Pravitha Pratheep <pratheepp@spu.edu>
2021-05-17 22:53:22 -0700

    AsianEggs update

--- screens/Asian.js
+++ screens/Asian.js
@@ -182,7 +182,7 @@ const ListItem = ({ item }) => {
     console.log(type);
     switch (type) {
       case "Milk":
-        // AsyncStorage.removeItem("@order_Key");
+        AsyncStorage.removeItem("@order_Key");
         navigation.navigate("AsianMilk");
         //navigate
         break;
